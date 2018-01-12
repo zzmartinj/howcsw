8b80d3c12a5baa2c5c920c9022277109efbd3ce0
zzmartinj@gmail.com <zzmartinj@gmail.com>
2018-01-11 19:35:25 -0500

    halfway through changing to Bing. Calling it a night

--- controllers/eventsearch.js
+++ controllers/eventsearch.js
@@ -1,21 +1,21 @@
 var http=require('http');
 
-function callGoogle(res, resp, query){
-    var googlehost="http://ajax.googleapis.com";
-    var googlepath="/ajax/services/search/web?v=1.0&q="
+function callBing(res, resp, query){
+    var bingHost='http://api.cognitive.microsoft.com';
+    var bingPath='/bing/v7.0/search?q=';
     var options={
-        host : googlehost,
+        host : bingHost,
         port : 80,
-        path : googlepath + query,
-        method : 'POST'
+        path : bingPath + query,
+        method : 'GET'
     };
 
     http.request(options,function(res){
-        console.log("STATUS:" + res.statusCode);
-        console.log("HEADERS:" + res.headers );
+        console.log('STATUS:' + res.statusCode);
+        console.log('HEADERS:' + res.headers );
         res.setEncoding('utf-8');
         res.on('data', function(data){
-            console.log("BODY:" + data);
+            console.log('BODY:' + data);
         })
     }).end();
 }
