3edb151ace4c40fe0b3b93e7615120ba5e2511ec
zzmartinj@gmail.com <zzmartinj@gmail.com>
2018-01-13 17:15:51 -0500

    changed key to a query param vs hardcoded in url

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ var router = express.Router();
 var eventsearch = require('../controllers/eventsearch'); //my own google api controller
 
 
-router.get('/:eventname/:key', function(req, res, next) {
+router.get('/:eventname', function(req, res, next) {
   var event=req.params.eventname;
-  var key=req.params.key;
+  //var key=req.params.key;
+  var key=req.query.key;
   eventsearch.callBing(res,res,event, key);  //call Bing over Azure!
   res.render('index', { title: 'Test! ' + event });
 });
