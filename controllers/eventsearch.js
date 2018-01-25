cefd635da50e74360092f8081ad5c7810033343b
zzmartinj@gmail.com <zzmartinj@gmail.com>
2018-01-24 19:25:36 -0500

    simplified the code to use the request library over http and https

--- controllers/eventsearch.js
+++ controllers/eventsearch.js
@@ -1,32 +1,5 @@
 var https = require('https'); // NOTE this is httpS, not http
-var request = require ('request');
-
-
-//My main handler to simply output what we get back from Bing. Over time, this has 
-//to return something vs just outputting it on the log.
-function processRequest(response) {
-    let body = '';
-    response.on('data', function (d) {
-        body += d;
-    });
-    response.on('end', function () {
-        console.log('\nRelevant Headers:\n');
-        for (var header in response.headers)
-            // header keys are lower-cased by Node.js
-            if (header.startsWith("bingapis-") || header.startsWith("x-msedge-"))
-                console.log(header + ": " + response.headers[header]);
-
-        let responseJSON = JSON.parse(body);
-        responseJSON = filterJSONResponse(responseJSON);
-        console.log('\nJSON Response:\n');
-        console.log(JSON.stringify(responseJSON, null, '    '));
-        return (responseJSON);
-    });
-    response.on('error', function (e) {
-        console.log('Error: ' + e.message);
-    });
-
-}
+var request = require('request');
 
 //basic function scoped out to filter our JSON received
 function filterJSONResponse(jsonToFilter) {
@@ -41,32 +14,39 @@ function filterJSONResponse(jsonToFilter) {
 
 }
 
-
+//My main handler to simply output what we get back from Bing. Over time, this has 
+//to return something vs just outputting it on the log.
+function processRequest(dataToParse) {
+    let body = dataToParse;
+    let responseJSON = JSON.parse(body);
+    responseJSON = filterJSONResponse(responseJSON);
+    console.log(JSON.stringify(responseJSON, null, '    '));
+    return (responseJSON);
+}
 
 //This is the main entry function. It sets up what's needed to make the request, then calls 
 //a function to process it and for now print it out (processRequest)
-function callBing(request, response, query, key) {
-    let host = 'api.cognitive.microsoft.com';
+function callBing(query, key) {
+    let host = 'https://api.cognitive.microsoft.com';
     let path = '/bing/v7.0/search';
 
     let answercount = '2';
     console.log('Searching the Web for: ' + query);
     let request_params = {
         method: 'GET',
-        hostname: host,
-        path: path + '?mkt=en-us&answerCount=' + answercount + '&q=' + encodeURIComponent(query),
+        url: host + path + '?mkt=en-us&answerCount=' + answercount + '&q=' + encodeURIComponent(query),
         headers: {
             'Ocp-Apim-Subscription-Key': key,
         }
     };
-    let results = '';
-    //NOTE THIS IS HTTPS, not HTTP    
-    var req = https.request(request_params, function (data) {
-        results = processRequest(data);
-        console.log('RESULTS: ' + results);        
-        return results;
-    }); 
-    req.end();
+
+    request(request_params, function (err, response, data) {
+        if (err) {
+            console.log("Error! " + err);
+        }
+        data = processRequest(data);
+        console.log(data);
+    });
 }
 
 module.exports.callBing = callBing;
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var https = require('https'); // NOTE this is httpS, not http
 var eventsearch = require('../controllers/eventsearch'); //my own google api controller
 
 
-router.get('/:eventname', function(req, res, next) {
-  var event=req.params.eventname;
-  //var key=req.params.key;
-  var key=req.query.key;
-  results=eventsearch.callBing(res,res,event, key);  //call Bing over Azure!
+router.get('/:eventname', function (req, res, next) {
+
+  var event = req.params.eventname;
+  var key = req.query.key;
+  
+  /*call Bing over Azure! */
+  eventsearch.callBing(event,key);  
   res.render('index', { title: 'Test! ' + event });
+
 });
+
 /* GET home page and show that you must supply an event. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   var err = new Error('Must supply an event!');
   err.status = 500;
   next(err);
